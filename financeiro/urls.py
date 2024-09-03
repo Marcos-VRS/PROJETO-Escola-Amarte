7014d6baadf4b49b62773aa5f0512bff92bc3211
Marcos <marcosvrsdevmail@gmail.com>
2024-09-03 02:52:49 -0300

    html dinâmico para troca de partials

--- financeiro/urls.py
+++ financeiro/urls.py
@@ -8,5 +8,11 @@ urlpatterns = [
     path("login/", views.tela_login.login_view, name="tela_login"),
     path("logout/", views.tela_login.logout_view, name="tela_logout"),
     # Menu lateral
+    path("index/", views.index.index, name="index"),
     path("dashboard/", views.index.dashboard, name="dashboard"),
+    path("transacoes/", views.index.transacoes, name="transacoes"),
+    path("aulas/", views.index.aulas, name="aulas"),
+    path("cadastro/", views.index.cadastro, name="cadastro"),
+    path("fiscal/", views.index.fiscal, name="fiscal"),
+    path("metas/", views.index.metas, name="metas"),
 ]
--- financeiro/views/index.py
+++ financeiro/views/index.py
@@ -3,5 +3,43 @@ from django.contrib.auth.decorators import login_required
 
 
 @login_required(login_url="financeiro:tela_login")
+def index(request):
+    return render(request, "financeiro/index.html", {"username": request.user.username})
+
+
+@login_required(login_url="financeiro:tela_login")
 def dashboard(request):
-    return render(request, "financeiro/index.html")
+    return render(
+        request, "financeiro/dashboard.html", {"username": request.user.username}
+    )
+
+
+@login_required(login_url="financeiro:tela_login")
+def transacoes(request):
+    return render(
+        request, "financeiro/transacoes.html", {"username": request.user.username}
+    )
+
+
+@login_required(login_url="financeiro:tela_login")
+def aulas(request):
+    return render(request, "financeiro/aulas.html", {"username": request.user.username})
+
+
+@login_required(login_url="financeiro:tela_login")
+def cadastro(request):
+    return render(
+        request, "financeiro/cadastro.html", {"username": request.user.username}
+    )
+
+
+@login_required(login_url="financeiro:tela_login")
+def fiscal(request):
+    return render(
+        request, "financeiro/fiscal.html", {"username": request.user.username}
+    )
+
+
+@login_required(login_url="financeiro:tela_login")
+def metas(request):
+    return render(request, "financeiro/metas.html", {"username": request.user.username})
